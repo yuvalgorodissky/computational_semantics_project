892ee648108db1d853bac286c76a2e58efaa3c20
noamazulay <noamaz@post.bgu.ac.il>
2024-06-27 16:19:59 +0300

    rewrite analyze_results.py add heatmaps

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -3,42 +3,43 @@ import json
 import pandas as pd
 
 base_path = "/Users/noamazulay/Desktop/studies/fourth year/semester b/computational_semantics_project"
-model_path = f"{base_path}/models/flan-t5-base"
+model_name = "flan-t5-base"
+model_path = f"{base_path}/models/{model_name}"
 # Get all the files in the model path
 train_datasets = os.listdir(model_path)
 # Sort the datasets by length
 train_datasets.sort(key=len)
 val_datasets = os.listdir(f"{model_path}/{train_datasets[0]}")
-metrics = ["average_exact_match", "average_f1", "exact_match_has_ans", "f1_has_ans", "exact_match_no_ans", "f1_no_ans"]
 
+val_datasets=[x[:-5] for x in val_datasets]
+## rename the datasets
+val_datasets = [x.replace('valid','squad2-devset') for x in val_datasets]
+val_datasets = [x.replace('english_dev_tydiqa_filterd', ' tydiqa_devset ') for x in val_datasets]
+val_datasets.sort(key=len)
+
+metrics = ["average_exact_match", "average_f1", "exact_match_has_ans", "f1_has_ans", "exact_match_no_ans", "f1_no_ans"]
 # Dictionary to store the data
 data = {train_dataset: {} for train_dataset in train_datasets}
 for metric in metrics:
     # Loop through the files and extract the metrics
     for train_dataset in train_datasets:
         for val_dataset in val_datasets:
-            with open(f"{model_path}/{train_dataset}/{val_dataset}/metrics.json", 'r') as f:
+            val_dataset_name= val_dataset
+            if val_dataset == "squad2-devset":
+                val_dataset_name = 'valid'
+            if val_dataset == ' tydiqa_devset ':
+                val_dataset_name = 'english_dev_tydiqa_filterd'
+            with open(f"{model_path}/{train_dataset}/{val_dataset_name}.json/metrics.json", 'r') as f:
                 content = json.load(f)[metric]
+
                 data[train_dataset][val_dataset] = content
 
     # Convert the dictionary to a DataFrame
     df = pd.DataFrame.from_dict(data, orient='index')
 
+    ## Create a dictionary to store the data
+    os.makedirs(f"{base_path}/eval/{model_name}/tables", exist_ok=True)
     # Save the DataFrame to an Excel file with conditional formatting
-    excel_filename = f"{base_path}/eval/metrics_{metric}.xlsx"
-    with pd.ExcelWriter(excel_filename, engine='xlsxwriter') as writer:
-        df.to_excel(writer, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-
-        # Define the conditional formatting rule
-        for col_num in range(1, df.shape[1] + 1):  # Starting from 1 to skip index column
-            color_scale_rule = {
-                'type': '2_color_scale',
-                'min_color': "#FF9999",  # Red
-                'max_color': "#99CCFF"  # Blue
-            }
-            # Apply the conditional formatting to each column individually
-            worksheet.conditional_format(1, col_num, df.shape[0], col_num, color_scale_rule)
-
-    print(f"Excel file '{excel_filename}' with conditional formatting has been created successfully.")
+    csv_filename = f"{base_path}/eval/{model_name}/tables/metrics_{metric}.csv"
+    df.to_csv(csv_filename)
+    print(f"Saved {csv_filename}")
---
+++ src/heatmap.py
@@ -0,0 +1,47 @@
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Function to read CSV and set the first column as index
+def read_and_process_csv(file_path):
+    data = pd.read_csv(file_path)
+    data.set_index(data.columns[0], inplace=True)
+    return data
+
+# Function to generate heatmap
+def generate_heatmap(data, title):
+   ## Create a figure with figsize
+    plt.figure(figsize=(16, 12))
+
+    sns.heatmap(data, annot=True, fmt=".1f", cmap="YlGnBu", cbar_kws={'label': 'Score'})
+    plt.title(title, fontsize=14)
+    plt.xticks(rotation=22)
+    plt.yticks(rotation=0)
+    plt.ylabel('Train Dataset')
+    plt.xlabel('Validation Dataset')
+    ## Save the heatmap as an image
+    os.makedirs(f"{directory_path}/{model_name}/heatmaps", exist_ok=True)
+    plt.savefig(f"{directory_path}/{model_name}/heatmaps/{title}.png")
+
+# List of CSV files and their titles
+csv_files = [
+    ('metrics_average_exact_match.csv', "Average Exact Match"),
+    ('metrics_average_f1.csv', "Average F1"),
+    ('metrics_exact_match_has_ans.csv', "Exact Match (Has Answer)"),
+    ('metrics_exact_match_no_ans.csv', "Exact Match (No Answer)"),
+    ('metrics_f1_has_ans.csv', "F1 Score (Has Answer)"),
+    ('metrics_f1_no_ans.csv', "F1 Score (No Answer)")
+]
+model_name = "flan-t5-base"
+
+
+# Path to the directory containing CSV files
+directory_path = '/Users/noamazulay/Desktop/studies/fourth year/semester b/computational_semantics_project/eval/'
+
+# Read each CSV file, process it, and generate the heatmap
+for file_name, title in csv_files:
+    file_path = f"{directory_path}/{model_name}/tables/{file_name}"
+    data = read_and_process_csv(file_path)
+    generate_heatmap(data, f"{title}\n{model_name}")
